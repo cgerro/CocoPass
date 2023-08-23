888582beafa948a4f2a75a68357d6cc6382ea4e2
gregolebo <gguyot1023@gmail.com>
2023-08-23 19:37:10 +0200

    test(helloworld)

--- cocopassflutter/lib/main.dart
+++ cocopassflutter/lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Hello, World2'),
+            const Text('Hello, World23'),
             const Text(
               'You have pushed the button this many times:',
             ),
