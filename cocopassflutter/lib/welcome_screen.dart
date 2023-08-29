60430f442120763b004cfc12a9dba38249eb8771
Céline Roger <roger.celine93@gmail.com>
2023-08-29 17:51:33 +0200

    fix(welcome_screen.dart): correction warning constructeur widget

--- cocopassflutter/lib/welcome_screen.dart
+++ cocopassflutter/lib/welcome_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 // import 'signup_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
