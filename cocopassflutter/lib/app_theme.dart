b67e2be96e5a22abfea900ac72ca1fc45249c8c9
Céline Roger <roger.celine93@gmail.com>
2023-08-30 10:11:04 +0200

    fix(app_theme.dart): Correction des warnings

--- cocopassflutter/lib/app_theme.dart
+++ cocopassflutter/lib/app_theme.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primarySeedColor = const Color(0xFF6f00ff);
-  static const Color secondarySeedColor = const Color(0xFF6750A4);
-  static const Color tertiarySeedColor = const Color(0xFF2B2930);
+  static Color primarySeedColor = const Color(0xFF6f00ff);
+  static Color secondarySeedColor = const Color(0xFF6750A4);
+  static Color tertiarySeedColor = const Color(0xFF2B2930);
 
   static ThemeData lightTheme() {
     return ThemeData(
