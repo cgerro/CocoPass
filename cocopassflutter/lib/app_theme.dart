f263e81a81ce64780bcdea1d1e0ad53640ab5fc1
Céline Roger <roger.celine93@gmail.com>
2023-08-31 11:16:19 +0200

    add(home_screen.dart): Ajout de la page d'accueil

--- cocopassflutter/lib/app_theme.dart
+++ cocopassflutter/lib/app_theme.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static Color primarySeedColor = const Color(0xFF6f00ff);
-  static Color secondarySeedColor = const Color(0xFF6750A4);
-  static Color tertiarySeedColor = const Color(0xFF2B2930);
+  // static Color primarySeedColor = const Color(0xFF6f00ff);
+  // static Color secondarySeedColor = const Color(0xFF6750A4);
+  // static Color tertiarySeedColor = const Color(0xFF2B2930);
 
   static ThemeData lightTheme() {
     return ThemeData(
         brightness: Brightness.dark,
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: primarySeedColor, brightness: Brightness.dark));
+        bottomNavigationBarTheme:
+            BottomNavigationBarThemeData(backgroundColor: Colors.grey[850])
+        // colorScheme: ColorScheme.fromSeed(seedColor: primarySeedColor)
+        );
   }
 }
